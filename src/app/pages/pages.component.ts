c6acba44895629e9764b2bbf04d2bcf563160942
gonzalomadariaga1 <gonzalo.madariaga@alumnos.uv.cl>
2024-06-23 11:47:43 -0400

    fix error declare function

--- src/app/pages/pages.component.ts
+++ src/app/pages/pages.component.ts
@@ -1,7 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { SettingsService } from '../services/settings.service';
 
-declare function customInitFunctions();
+declare function customInitFunctions(): void;
 
 @Component({
   selector: 'app-pages',
